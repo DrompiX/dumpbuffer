ab3e4bac5b1981b0ea663b54b91f4acab9d9df19
DrompiX <mrlocky98@gmail.com>
2021-09-26 21:39:43 +0300

    Move cli to ui module and write tests (#2)
    * Add cli tests and transfer cli in ui module
    
    * Add tests for list cli command

---
+++ src/ui/cli.rs
@@ -0,0 +1,118 @@
+use std::ffi::OsString;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt, Clone, PartialEq)]
+pub enum DumpBufferCLI {
+    #[structopt(setting = structopt::clap::AppSettings::TrailingVarArg)]
+    Add {
+        key: String,
+        #[structopt(required = true)]
+        value: Vec<OsString>,
+    },
+    Get {
+        key: String,
+    },
+    List {
+        #[structopt(long)]
+        keys_only: bool,
+    },
+}
+
+impl DumpBufferCLI {
+    #[allow(dead_code)]
+    pub fn joined_value(&self, separator: &str) -> Option<String> {
+        match self {
+            DumpBufferCLI::Add { key: _, value } => {
+                let vals: Vec<String> = value
+                    .iter()
+                    .map(|v| {
+                        let result_string = v.to_owned().into_string();
+                        result_string.expect(format!("Cant parse value {:?}", v).as_str())
+                    })
+                    .collect();
+                Some(vals.join(separator))
+            }
+            _ => None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn add_works_with_key_value() {
+        assert_eq!(
+            DumpBufferCLI::from_iter(&["test", "add", "some-key", "some-value"]),
+            DumpBufferCLI::Add {
+                key: "some-key".to_string(),
+                value: [OsString::from("some-value")].to_vec()
+            }
+        );
+    }
+
+    #[test]
+    fn add_works_when_value_has_flags() {
+        assert_eq!(
+            DumpBufferCLI::from_iter(&["test", "add", "key", "value", "-c", "hey"]),
+            DumpBufferCLI::Add {
+                key: "key".to_string(),
+                value: [
+                    OsString::from("value"),
+                    OsString::from("-c"),
+                    OsString::from("hey")
+                ]
+                .to_vec()
+            }
+        );
+    }
+
+    #[test]
+    fn get_works_with_key() {
+        assert_eq!(
+            DumpBufferCLI::from_iter(&["test", "get", "key-name"]),
+            DumpBufferCLI::Get {
+                key: "key-name".to_string()
+            }
+        );
+    }
+
+    #[test]
+    fn list_is_parsed_correctly_without_flag() {
+        assert_eq!(
+            DumpBufferCLI::from_iter(&["test", "list"]),
+            DumpBufferCLI::List { keys_only: false }
+        );
+    }
+    
+    #[test]
+    fn list_is_parsed_correctly_with_flag() {
+        assert_eq!(
+            DumpBufferCLI::from_iter(&["test", "list", "--keys-only"]),
+            DumpBufferCLI::List { keys_only: true }
+        );
+    }
+
+    #[test]
+    fn joined_value_works_correctly() {
+        let v = DumpBufferCLI::from_iter(&["test", "add", "key", "bash", "-c", "hey"]);
+        assert_eq!(v.joined_value(" "), Some("bash -c hey".to_string()));
+    }
+
+    #[test]
+    fn joined_value_accepts_different_separators() {
+        let v = DumpBufferCLI::from_iter(&["test", "add", "key", "bash", "-c", "hey"]);
+        assert_eq!(v.joined_value("|"), Some("bash|-c|hey".to_string()));
+        assert_eq!(v.joined_value("?"), Some("bash?-c?hey".to_string()));
+        assert_eq!(v.joined_value("*"), Some("bash*-c*hey".to_string()));
+    }
+
+    #[test]
+    fn joined_value_is_none_for_other_commands() {
+        let get_cmd = DumpBufferCLI::Get { key: "any".to_string() };
+        let list_cmd = DumpBufferCLI::List { keys_only: false };
+        assert_eq!(get_cmd.joined_value(" "), None);
+        assert_eq!(list_cmd.joined_value(" "), None);
+    }
+}
