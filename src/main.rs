c5c76478a4e31b9fb65584d9900fcda034ae729d
Dima T <mrlocky98@gmail.com>
2021-09-26 21:33:59 +0300

    Add cli tests and transfer cli in ui module

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,9 @@
+mod ui;
+
+use crate::ui::cli;
 use structopt::StructOpt;
 
-#[derive(Debug, StructOpt, Clone)]
-enum DumpBufferCLI {
-    #[structopt(setting = structopt::clap::AppSettings::TrailingVarArg)]
-    Add {
-        key: String,
-        value: Vec<std::ffi::OsString>,
-    },
-    Get {
-        key: String,
-    },
-    List {
-        #[structopt(long)]
-        keys_only: bool,
-    },
-}
-
-impl DumpBufferCLI {
-    fn joined_value(&self, separator: Option<&str>) -> Option<String> {
-        let sep = separator.unwrap_or(" ");
-        match self {
-            DumpBufferCLI::Add { key: _, value } => {
-                let vals: Vec<String> = value
-                    .iter()
-                    .map(|v| {
-                        let result_string = v.to_owned().into_string();
-                        result_string.expect(format!("Cant parse value {:?}", v).as_str())
-                    })
-                    .collect();
-                Some(vals.join(sep))
-            }
-            _ => None,
-        }
-    }
-}
-
 fn main() {
-    let args = DumpBufferCLI::from_args();
-    println!("{}", args.joined_value(None).unwrap());
+    let args = cli::DumpBufferCLI::from_args();
+    println!("{}", args.joined_value(" ").unwrap());
 }
