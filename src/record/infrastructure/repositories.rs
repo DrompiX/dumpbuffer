117fcb4fc0c269bbc4a3d47b52fdf8c5fcabd6b2
Dima T <mrlocky98@gmail.com>
2021-10-02 15:27:16 +0300

    Implement kv file database with add method and add for repository. It works, haha!

--- src/record/infrastructure/repositories.rs
+++ src/record/infrastructure/repositories.rs
@@ -1,8 +1,11 @@
+#![allow(dead_code)]
+
 use std::cell::RefCell;
 use std::collections::HashMap;
 
 use crate::record::domain::entities::Record;
 use crate::record::domain::repositories::RecordRepository;
+use crate::shared::infrastructure::file_db::KVFileDatabase;
 
 /// Repository to store records in memory. Primary use case - testing
 pub struct InMemoryRecordRepository {
@@ -29,7 +32,7 @@ impl RecordRepository for InMemoryRecordRepository {
     fn get(&self, key: String) -> Result<Record, String> {
         match self.storage.borrow().get(&key).clone() {
             Some(v) => Ok(Record { key, value: v.to_string() }),
-            None => Err(String::from(format!("Record with key \"{}\" does not exist", key)))
+            None => Err(format!("Record with key \"{}\" does not exist", key))
         }
     }
 
@@ -37,5 +40,30 @@ impl RecordRepository for InMemoryRecordRepository {
         self.storage.borrow().iter()
             .map(|(k, v)| Record::new(k, v))
             .collect()
+    }
+}
+
+////////////////////////////////////////////
+pub struct KVFileDatabaseRepository<'a> {
+    storage: &'a KVFileDatabase,
+}
+
+impl<'a> KVFileDatabaseRepository<'a> {
+    pub fn new(storage: &'a KVFileDatabase) -> Self {
+        KVFileDatabaseRepository { storage }
+    }
+}
+
+impl<'a> RecordRepository for KVFileDatabaseRepository<'a> {
+    fn add(&self, record: Record) {
+        self.storage.add(&record.key, &record.value).unwrap();
+    }
+
+    fn get(&self, key: String) -> Result<Record, String> {
+        todo!()
+    }
+
+    fn all(&self) -> Vec<Record> {
+        todo!()
     }
 }
